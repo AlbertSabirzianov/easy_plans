d82bf7d6310a102faa2a9530a2135cfb0ee6c973
AlbertSabirzianov <albertuno@mail.ru>
2024-05-01 13:58:05 +0300

    Добавил функционал по добавлению и удалению Годов обучения в Планах

--- easy_plans/plans/urls.py
+++ easy_plans/plans/urls.py
@@ -6,5 +6,7 @@ app_name = 'plans'
 
 urlpatterns = [
     path('update/<int:student_pk>', views.PlanUpdateView.as_view(), name='update'),
-    path('list/', views.PlanList.as_view(), name='list')
+    path('year/<int:year_id>', views.YearUpdateView.as_view(), name='year'),
+    path('year_create/<int:plan_id>', views.YearCreateView.as_view(), name='year_create'),
+    path('year_delete/<int:year_id>', views.YearDelete.as_view(), name='year_delete')
 ]
--- easy_plans/plans/views.py
+++ easy_plans/plans/views.py
@@ -1,15 +1,29 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
-from django.urls import reverse_lazy
-from django.views.generic import UpdateView, ListView
+from typing import Sequence
 
-from plans.models import Plan
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, View, CreateView
+
+from plans.models import Plan, Year
 from users.models import Student
 
 
 class PlanUpdateView(UpdateView, LoginRequiredMixin):
     model = Plan
     fields = ('department', 'section')
+
+    def __get_student(self) -> Student:
+        student = Student.objects.filter(pk=int(self.kwargs.get('student_pk'))).first()
+        if not student:
+            raise Http404()
+        return student
+
+    def __get_years(self) -> Sequence[Year]:
+        years = Year.objects.filter(
+            plan=self.object
+        )
+        return years
 
     def get_success_url(self):
         return reverse_lazy(
@@ -18,21 +32,78 @@ class PlanUpdateView(UpdateView, LoginRequiredMixin):
         )
 
     def get_object(self, queryset=None) -> Plan:
-        student = Student.objects.filter(pk=int(self.kwargs.get('student_pk'))).first()
-        if not student:
-            raise Http404()
+        student = self.__get_student()
         plan = self.model.objects.filter(
             student=student
         ).first()
         return plan
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['years'] = self.__get_years()
+        return context
 
-class PlanList(ListView, LoginRequiredMixin):
-    model = Plan
 
-    def get_queryset(self):
-        query = self.model.objects.filter(
-            student__work_place__user=self.request.user
+class YearDelete(View, LoginRequiredMixin):
+
+    def get_success_url(self):
+        return reverse_lazy(
+            'plans:update',
+            kwargs={'student_pk': self.year.plan.pk}
         )
-        return query
+
+    def __get_year(self):
+        self.year = Year.objects.filter(
+            pk=int(self.kwargs.get('year_id'))
+        ).first()
+        if not self.year:
+            raise Http404()
+        return self.year
+
+    def get(self, request, year_id):
+        self.__get_year().delete()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class YearCreateView(CreateView, LoginRequiredMixin):
+    """
+    TODO: update and saving with modelformset Concert, Exam, Quarter
+    """
+
+    model = Year
+    template_name = 'plans/year_create_form.html'
+    fields = ('start_year', 'end_year', 'result', 'characteristic')
+
+    def __get_plan(self) -> Plan:
+        plan = Plan.objects.filter(
+            pk=int(self.kwargs.get('plan_id'))
+        ).first()
+        if not plan:
+            raise Http404()
+        return plan
+
+    def form_valid(self, form):
+        form.instance.plan = self.__get_plan()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy(
+            'plans:update',
+            kwargs={'student_pk': self.__get_plan().pk}
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['plan_id'] = self.kwargs.get('plan_id')
+        return context
+
+
+class YearUpdateView(UpdateView, LoginRequiredMixin):
+    """
+    TODO: update and saving with modelformset Concert, Exam, Quarter
+    """
+    model = Year
+    fields = ('start_year', 'end_year', 'result', 'characteristic')
+    pk_url_kwarg = 'year_id'
+
 
